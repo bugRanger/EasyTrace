6554ebb4a97b11f8dff06c81b19e8749bf730782
bugRanger <simpledowork@gmail.com>
2026-05-20 18:28:34 +0700

    Add POC

---
+++ src/EasyTrace/TraceRef.cs
@@ -0,0 +1,38 @@
+﻿namespace EasyTrace;
+
+public class TraceProvider
+{
+    // TODO: Add sampler.
+    // TODO: Add batcher.
+    // TODO: Add exporters.
+}
+
+public ref struct TraceRef(TraceProvider provider, Trace? trace) : IDisposable
+{
+    public void Dispose()
+    {
+        // TODO: flush current trace.
+    }
+}
+
+public class Trace
+{
+}
+
+public ref struct TraceData: IDisposable
+{
+    public TraceRef Next()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        // TODO: Unlock all slots in circle buffer.
+    }
+}
+
+public interface TraceExporter
+{
+    void Export(scoped TraceData traceData);
+}
