59303a1970f19097f7910bfe30eb03336fd87746
bugRanger <simpledowork@gmail.com>
2026-09-01 17:37:59 +0700

    Add activity interceptor

---
+++ src/EasyTrace/Interceptor/ITraceActivityInterceptor.cs
@@ -0,0 +1,9 @@
+﻿using EasyTrace.Activity;
+
+namespace EasyTrace.Interceptor;
+
+public interface ITraceActivityInterceptor
+{
+    void Start(scoped in TraceActivityRef activityRef);
+    void Stop(scoped in TraceActivityRef activityRef);
+}
